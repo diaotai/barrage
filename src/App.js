77a4cb31431ebe15aaa10b536669851cd9673656
diao <diaotai123@gmail.com>
2017-07-24 22:26:04 +0800

    成功让文字从右滑行至左边不见

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <span className="barr">hello world</span>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
